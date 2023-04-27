6b46833cc934316ac9715e567189cdaee65fa460
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-04-27 17:36:16 +0700

    fix bug

---
+++ int221-nw1-saksionchowder/src/main/java/sit/int221/int221nw1saksionchowder/Int221Nw1SaksionchowderApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.int221nw1saksionchowder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int221Nw1SaksionchowderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Int221Nw1SaksionchowderApplication.class, args);
+    }
+
+}
---
+++ int221-nw1-saksionchowder/src/test/java/sit/int221/int221nw1saksionchowder/Int221Nw1SaksionchowderApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int221.int221nw1saksionchowder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int221Nw1SaksionchowderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
