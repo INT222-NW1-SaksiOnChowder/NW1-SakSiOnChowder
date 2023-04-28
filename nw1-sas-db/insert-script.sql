663846980e980dfb2021257d9b1ace978e4a5e66
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-04-28 16:02:25 +0700

    change db

--- nw1-sas-db/insert-script.sql
+++ nw1-sas-db/insert-script.sql
@@ -1,19 +1,19 @@
-use sas;
+use SAS;
 
-insert into categories values ();
-insert into categories(categoryName) values ("ทุนการศึกษา");
-insert into categories(categoryName) values ("หางาน");
-insert into categories(categoryName) values ("ฝึกงาน");
+insert into Categories values ();
+insert into Categories(categoryName) values ("ทุนการศึกษา");
+insert into Categories(categoryName) values ("หางาน");
+insert into Categories(categoryName) values ("ฝึกงาน");
 
-insert into announcements(announcementTitle,announcementDescription,announcementDisplay,categoryId) 
-values ('บริษัท เน็ตเซอร์พลัส จํากัด รับสมังาน 2 ตําแหน่ง','บริษัท เน็ตเซอร์พลัส จํากัด เปิดรับสมัครงาน 2 ตําแหน่ง Application Support และ Customer Support','N',1);
+insert into Announcements(announcementTitle,announcementDescription,announcementDisplay,categoryId) 
+values ('บริษัท เน็ตเซอร์พลัส จํากัด รับสมัครงาน 2 ตําแหน่ง','บริษัท เน็ตเซอร์พลัส จํากัด เปิดรับสมังาน 2 ตําแหน่ง Application Support และ Customer Support','N',1);
 
-insert into announcements(announcementTitle,announcementDescription,closeDate,announcementDisplay,categoryId) 
+insert into Announcements(announcementTitle,announcementDescription,closeDate,announcementDisplay,categoryId) 
 values ('รายชื่อนักศึกษาที่ได้รับทุนการศึกษาประเภท "ทุนจ้างงาน" 2/2565','คณะ ฯ ประกาศรายชื่อนักศึกษาที่ได้รับทุนการศึกษาประเภท "ทุนจ้างงาน" ประจําภาคการศึกษา 2/2565','2023-05-31 11:00:00','Y',2);
 
-insert into announcements(announcementTitle,announcementDescription,publishDate,announcementDisplay,categoryId) 
+insert into Announcements(announcementTitle,announcementDescription,publishDate,announcementDisplay,categoryId) 
 values ('แนวปฎิบัติการสอบออนไลน์ พ.ศ. 2565','ประกาศมหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี เรื่องแนวทางปฎิบัติการสอบออนไลน์ พ.ศ. 2565', '2023-01-26 23:00:00','Y',3);
 
-insert into announcements(announcementTitle,announcementDescription,publishDate,closeDate,announcementDisplay,categoryId) 
+insert into Announcements(announcementTitle,announcementDescription,publishDate,closeDate,announcementDisplay,categoryId) 
 values ('กิจกรรมพี่อ้อย พี่ฉอด On Tour 2566','ขอเชิญนักศึกษาทุกชั้นปี เข้าร่วมกิจกรรมพี่อ้อย พี่ฉอด On Tour','2023-04-18 23:00:00','2023-05-08 11:00:00','Y',3);
 
