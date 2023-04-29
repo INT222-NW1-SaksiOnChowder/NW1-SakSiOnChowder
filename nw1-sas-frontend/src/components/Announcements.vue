07d5e1ae2caa21969b9530640c556f7ce5636433
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-04-29 15:11:54 +0700

    change2

--- nw1-sas-frontend/src/components/Announcements.vue
+++ nw1-sas-frontend/src/components/Announcements.vue
@@ -11,7 +11,6 @@ onMounted(async () => {
   announcements.value = await getAnnouncements();
 });
 
-// detail
 const showDetail = ref(true);
 const idDetail = (id) => {
   showDetail.value = !showDetail.value;
