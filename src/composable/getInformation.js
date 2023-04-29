3587a2947fdcc2c8fc8292755fb76df0399421f5
Kamolphon <kamolphon.thon@kmutt.ac.th>
2023-04-29 14:50:51 +0700

    edit variables

--- src/composable/getInformation.js
+++ src/composable/getInformation.js
@@ -2,9 +2,9 @@ const getAnnouncements= async () => {
     try {
         const res = await fetch(`http://ip22nw1.sit.kmutt.ac.th:8080/api/announcements`)
         if (res.status === 200) {
-            const user = await res.json()
+            const announcement = await res.json()
             console.log('All Successfully')
-            return user
+            return announcement
         } else {
             throw new Error('No announcement')
         }
@@ -17,9 +17,9 @@ const getAnnouncement= async (id) => {
     try {
         const res = await fetch(`http://ip22nw1.sit.kmutt.ac.th:8080/api/announcements/${id}`)
         if (res.status === 200) {
-            const user = await res.json()
+            const announcement = await res.json()
             console.log('Detail Successfully')
-            return user
+            return announcement
         } else {
             throw new Error(`Announcement id ${id} doesn't exist`)
         }
