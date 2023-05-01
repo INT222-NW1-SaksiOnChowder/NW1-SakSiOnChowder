bc1b68b7d9a1cc54bd48cecb0f8bf1b98f37fe54
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-05-01 12:09:10 +0700

    add file

---
+++ nw1-api-sas/src/main/java/sit/int221/nw1apisas/Nw1ApiSasApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.nw1apisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nw1ApiSasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nw1ApiSasApplication.class, args);
+    }
+
+}
