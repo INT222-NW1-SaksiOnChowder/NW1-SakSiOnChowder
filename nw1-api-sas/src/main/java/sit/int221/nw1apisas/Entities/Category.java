f15f069c3910abd29836a14401fb20181cfec61c
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-05-02 20:15:27 +0700

    category

--- nw1-api-sas/src/main/java/sit/int221/nw1apisas/Entities/Category.java
+++ nw1-api-sas/src/main/java/sit/int221/nw1apisas/Entities/Category.java
@@ -1,6 +1,7 @@
 package sit.int221.nw1apisas.Entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
