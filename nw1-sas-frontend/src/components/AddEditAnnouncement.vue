88f567b11ea17bb89c94a79d6a31ec665845392d
pupha2546 <pupha.2003@mail.kmutt.ac.th>
2023-05-02 23:14:23 +0700

    datetime

--- nw1-sas-frontend/src/components/AddEditAnnouncement.vue
+++ nw1-sas-frontend/src/components/AddEditAnnouncement.vue
@@ -1,29 +1,36 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-const updatedAnnouncement = ref('')
-const a = ref(1)
+
+const updatedAnnouncement = ref({
+    announcementTitle: "",
+    announcementDescription: "",
+    publishDate: "",
+    closeDate: "",
+    announcementDisplay: "",
+    categoryId: {
+        categoryId: 1,
+        categoryName: ""
+    }
+})
 const props = defineProps({
     announcement: { type: Object }
 })
 
-onMounted(() => {
-    // if (props.announcement === undefined) {
-        //add
-        updatedAnnouncement.value = {
-            announcementTitle: "ทีสุดหล่อ",
-            announcementDescription: "",
-            publishDate: "",
-            closeDate: "",
-            announcementDisplay: "",
-            categoryId: 1,
-            categoryName: ""
-        }
-        const ddd = ref(updatedAnnouncement.value.categoryId.categoryName)
-    // } else {
-    //     //edit
-    //     updatedAnnouncement.value = props.announcement
-    // }
-})
+const time = ref()
+const time2 = ref()
+
+const timer = () => {
+    const x = time2 + ':00'
+    const y = time + ' '+ x
+    console.log(y);
+    const ict = 7*60*60*1000
+    const date = new Date(y)
+    date.setTime(date.getTime()+ict)
+    console.log(date);
+    date.toISOString()
+    console.log(date);
+
+}
 
 </script>
  
@@ -40,27 +47,29 @@ onMounted(() => {
         </div>
         <div class="my-3">
             <label class="">Catagory</label><br>
-            <select class="bg-gray-400 w-2/5 rounded-sm" v-model="updatedAnnouncement.categoryId">
+            <select class="bg-gray-400 w-2/5 rounded-sm" v-model="updatedAnnouncement.categoryId.categoryId">
                 <option value="1">ทั่วไป</option>
                 <option value="2">ทุนการศึกษา</option>
                 <option value="3">หางาน</option>
                 <option value="4">ฝึกงาน</option>
             </select>
-            {{ updatedAnnouncement.categoryId }}
+            {{ updatedAnnouncement.categoryId.categoryId }}
         </div>
         <div class="my-3">
             <label>Description</label><br>
-            <textarea class="bg-gray-400 w-full rounded-sm" name="desc" id="three" cols="100" rows="5" v-model="updatedAnnouncement.announcementDescription"></textarea>
+            <textarea class="bg-gray-400 w-full rounded-sm" name="desc" id="three" cols="100" rows="5"
+                v-model="updatedAnnouncement.announcementDescription"></textarea>
         </div>
         <div class="my-3">
             <label>Publish Date</label><br>
-            <input class="bg-gray-400 w-1/5 mr-5 rounded-sm" type="date" v-model="updatedAnnouncement.announcementTitle">
-            <input class="bg-gray-400 w-1/5 rounded-sm" type="time" v-model="updatedAnnouncement.announcementTitle">
+            <input class="bg-gray-400 w-1/5 mr-5 rounded-sm" type="date" v-model="time">
+            <input class="bg-gray-400 w-1/5 rounded-sm" type="time" v-model="time2">
+            {{ timer()}}
         </div>
         <div class="my-3">
             <label>Close Date</label><br>
-            <input class="bg-gray-400 w-1/5 mr-5 rounded-sm" type="date">
-            <input class="bg-gray-400 w-1/5 rounded-sm" type="time">
+            <input class="bg-gray-400 w-1/5 mr-5 rounded-sm" type="date" v-model="updatedAnnouncement.closeDate">
+            <input class="bg-gray-400 w-1/5 rounded-sm" type="time" v-model="updatedAnnouncement.closeDate">
         </div>
         <div>
             <label>Display</label><br>
