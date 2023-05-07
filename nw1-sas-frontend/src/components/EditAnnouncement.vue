f5382e90da920efb300f8464e0329e6b1af1b99e
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-05-07 18:55:31 +0700

    test cypress

--- nw1-sas-frontend/src/components/EditAnnouncement.vue
+++ nw1-sas-frontend/src/components/EditAnnouncement.vue
@@ -5,18 +5,14 @@ import { useRoute, useRouter } from 'vue-router'
 import { updateAnnouncement } from '../composable/editAnnouncement'
 
 const announcementObj = ref({})
-const beforeAnnouncement = ref({})
+
 
 onMounted(async () => {
     const route = useRoute()
     announcementObj.value = await getAnnouncement(route.params.id)
-    beforeAnnouncement.value = await getAnnouncement(route.params.id)
     announcementObj.value.announcementDisplay = showAnnouncementDisplay(announcementObj.value)
-    beforeAnnouncement.value.announcementDisplay = showAnnouncementDisplay(beforeAnnouncement.value)
     setTime(announcementObj.value)
-    beforeAnnouncementSetTime(beforeAnnouncement.value)
     showAnnouncementCategory(announcementObj.value)
-    showAnnouncementCategory(beforeAnnouncement.value)
 })
 
 const router = useRouter()
@@ -26,41 +22,20 @@ const selectedPublishTime = ref('')
 const selectedCloseDate = ref('')
 const selectedCloseTime = ref('')
 
-const beforeAnnouncementPublishDate = ref('')
-const beforeAnnouncementPublishTime = ref('')
-const beforeAnnouncementCloseDate = ref('')
-const beforeAnnouncementCloseTime = ref('')
-
 const isDisabled = ref(true)
 
 const setButton = () =>{
     isDisabled.value = false
 }
 
-// const checkAnnouncement = computed(() => {
-//     if (announcementObj.value.announcementTitle === beforeAnnouncement.value.announcementTitle &&
-//         announcementObj.value.announcementDescription === beforeAnnouncement.value.announcementDescription &&
-//         announcementObj.value.publishDate === beforeAnnouncement.value.publishDate &&
-//         announcementObj.value.closeDate === beforeAnnouncement.value.closeDate &&
-//         announcementObj.value.announcementDisplay === beforeAnnouncement.value.announcementDisplay &&
-//         announcementObj.value.announcementCategory === beforeAnnouncement.value.announcementCategory) {
-//         if (selectedPublishDate.value === beforeAnnouncementPublishDate.value &&
-//             selectedPublishTime.value === beforeAnnouncementPublishTime.value &&
-//             selectedCloseDate.value === beforeAnnouncementCloseDate.value &&
-//             selectedCloseTime.value === beforeAnnouncementCloseTime.value) {
-//             return true
-//         } else {
-//             if (selectedPublishTime.value !== '' && selectedPublishDate.value === '' ||
-//                 selectedCloseTime.value !== '' && selectedCloseDate.value === '') {
-//                 return true
-//             } else{
-//                 return false
-//             }
-//         }
-//     } else {
-//         return false
-//     }
-// })
+const checkAnnouncement = computed(() => {
+            if (selectedPublishTime.value !== '' && selectedPublishDate.value === '' ||
+                selectedCloseTime.value !== '' && selectedCloseDate.value === '') {
+                return true
+            } else{
+                return false
+            }
+})
 
 const showAnnouncementDisplay = (announcement) => {
     if (announcement.announcementDisplay === "Y") {
@@ -84,35 +59,6 @@ const showAnnouncementCategory = (announcement) => {
         case 'ฝึกงาน':
             announcement.announcementCategory = 4
             break;
-    }
-}
-const beforeAnnouncementSetTime = (announcement) => {
-    if (announcement.publishDate !== null) {
-        // PublishDate
-        const publishDateTime = new Date(announcement.publishDate)
-
-        // selectedPublishDate
-        const publishDay = `${publishDateTime.getUTCDate() + 1 < 10 ? "0" : ""}${publishDateTime.getUTCDate() + 1}`
-        const publishMonth = `${publishDateTime.getMonth() + 1 < 10 ? "0" : ""}${publishDateTime.getMonth() + 1}`
-        const publishYear = publishDateTime.getFullYear()
-        beforeAnnouncementPublishDate.value = `${publishYear}-${publishMonth}-${publishDay}`
-
-        // selectedPublishTime
-        beforeAnnouncementPublishTime.value = `${publishDateTime.getHours() < 10 ? "0" : ""}${publishDateTime.getHours()}:${publishDateTime.getMinutes() < 10 ? "0" : ""}${publishDateTime.getMinutes()}`
-    }
-
-    if (announcement.closeDate !== null) {
-        //CloseDate
-        const closeDateTime = new Date(announcement.closeDate)
-
-        // selectedCloseDate
-        const closeDay = `${closeDateTime.getUTCDate() + 1 < 10 ? "0" : ""}${closeDateTime.getUTCDate() + 1}`
-        const closeMonth = `${closeDateTime.getMonth() + 1 < 10 ? "0" : ""}${closeDateTime.getMonth() + 1}`
-        const closeYear = closeDateTime.getFullYear()
-        beforeAnnouncementCloseDate.value = `${closeYear}-${closeMonth}-${closeDay}`
-
-        // selectedCloseTime
-        beforeAnnouncementCloseTime.value = `${closeDateTime.getHours() < 10 ? "0" : ""}${closeDateTime.getHours()}:${closeDateTime.getMinutes() < 10 ? "0" : ""}${closeDateTime.getMinutes()}`
     }
 }
 
@@ -179,14 +125,13 @@ const submitEdit = async (announcement) => {
     }
 
     // announcementCategory กับ categoryId
-    if (!editAnnouncement.announcementTitle || !editAnnouncement.announcementDescription || !editAnnouncement.categoryId) {
-        alert('cannot edit data')
-    } else {
+    
         console.log(selectedPublishDate.value);
         console.log(selectedCloseDate.value);
         await updateAnnouncement(editAnnouncement)
+        console.log(editAnnouncement)
         router.push({ name: 'announcements' })
-    }
+
 }
 
 </script>
@@ -239,12 +184,11 @@ const submitEdit = async (announcement) => {
         <div class="my-5">
             <router-link :to="{ name: 'announcementDetail' }">
                 <button
-                    class="rounded-md bg-gray-300 px-5 py-2 font-semibold hover:bg-amber-100">Back</button></router-link>
+                    class="ann-button rounded-md bg-gray-300 px-5 py-2 font-semibold hover:bg-amber-100">Back</button></router-link>
             <button
-                :disabled="isDisabled || !announcementObj.announcementTitle || !announcementObj.announcementDescription"
+                :disabled="isDisabled || checkAnnouncement "
                 class="ann-button ml-5 font-semibold rounded-md px-3 py-2 buttonEdit"
-                :style="isDisabled || !announcementObj.announcementTitle || !announcementObj.announcementDescription ? 'opacity: 0.5; background-color:lightgray; cursor: not-allowed;' : 'opacity: 1; background-color:lightgreen;'"
-                @click="submitEdit(announcementObj)">Submit</button>
+                @click="submitEdit(announcementObj)">submit</button>
 
         </div>
     </div>
