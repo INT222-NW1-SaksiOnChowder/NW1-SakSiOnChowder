086d3816227af8465936635beb81110c51eb0d6e
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-05-15 18:40:43 +0700

    Decoration

--- nw1-sas-frontend/src/components/AnnouncementDetail.vue
+++ nw1-sas-frontend/src/components/AnnouncementDetail.vue
@@ -21,57 +21,57 @@ onMounted(async() => {
 </script>
 
 <template>
-    <div>
-        <div class="mt-3">
-            <h1 class="text-2xl ml-5 font-bold">
+    <div class="text-center bg-Background w-screen h-screen">
+        <div class="rounded-full bg-DarkBlue inline-block mt-3">
+            <h1 class="text-BlueFonts text-2xl px-5 py-5 font-bold">
                 Announcement Detail:
             </h1>
         </div>
-        <div class="border m-5">
-            <div class="flex my-5">
-                <h1 class="mx-5">
+        <div class="rounded-lg bg-LightBlue m-10 py-3">
+            <div class="flex my-5 justify-center">
+                <h1 class="mx-5 font-bold">
                     Title
                 </h1>
                 <p class="ann-title">{{ announcement.announcementTitle }}</p>
             </div>
             <div class="flex my-5">
-                <h1 class="mx-5">
+                <h1 class="mx-5 font-bold">
                     Category
                 </h1>
                 <p class="ann-category">{{ announcement.announcementCategory }}</p>
             </div>
             <div class="flex my-5">
-                <h1 class="mx-5">
+                <h1 class="mx-5 font-bold">
                     Description
                 </h1>
                 <p class="ann-description">{{ announcement.announcementDescription }}</p>
             </div>
             <div class="flex my-5">
-                <h1 class="mx-5">
+                <h1 class="mx-5 font-bold">
                     Publish Date
                 </h1>
                 <p class="ann-publish-date">{{ changeDateTimeFormat(announcement.publishDate) }}</p>
             </div>
             <div class="flex my-5">
-                <h1 class="mx-5">
+                <h1 class="mx-5 font-bold">
                     Close Date
                 </h1>
                 <p class="ann-close-date">{{ changeDateTimeFormat(announcement.closeDate) }}</p>
             </div>
             <div class="flex my-5">
-                <h1 class="mx-5">
+                <h1 class="mx-5 font-bold">
                     Display
                 </h1>
                 <p class="ann-display">{{ announcement.announcementDisplay }}</p>
             </div>
         </div>
         <router-link :to="{ name: 'announcements' }"><button
-                class="ann-button bg-gray-200 rounded-md py-2 px-5 ml-5 hover:bg-amber-100"
+                class="ann-button bg-DarkRed rounded-full py-2 px-5 ml-5 hover:text-xl"
                 @click="idDetail">Back</button>
         </router-link>
 
         <router-link :to="{ name: 'editAnnouncement', params: { id: route.params.id } }">
-            <button class="ann-button bg-gray-200 rounded-md py-2 px-5 ml-5 hover:bg-emerald-200">Edit</button>
+            <button class="ann-button bg-DarkGreen rounded-full py-2 px-6 ml-5 hover:text-xl">Edit</button>
         </router-link>
 
 
--- nw1-sas-frontend/tailwind.config.js
+++ nw1-sas-frontend/tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    colors: {
+      "Background" : "#E4E5E9",
+      "LightBlue" : "#D1D8DE",
+      "DarkBlue" : "#A0B1C1",
+      "DarkGreen" : "#999B86",
+      "DarkRed" : "#BD6666",
+      "BlueFonts" : "#1C3150",
+      "Cream" : "#F0F0F0",
+      "ButtonViewHover" : "#C7C0B6",
+      "ButtonDeleteHover" : "#DCACAC"
+    }
   },
   plugins: [require("daisyui")],
 }
