3b09b1d57a9ff34303a1ee50ddc9484c14ca64e5
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-08-22 00:46:00 +0700

    fix PBI20-6

--- nw1-sas-frontend/src/components/users/UserEditDetail.vue
+++ nw1-sas-frontend/src/components/users/UserEditDetail.vue
@@ -47,17 +47,17 @@ const save = async (user) => {
                     <div class="my-5">
                         <label class="font-bold">Username</label><br>
                         <input maxlength="200" class="ann-username bg-InputColor drop-shadow-md h-8 w-full rounded-lg"
-                            type="text" v-model.trim="userObj.username">
+                            type="text" v-model="userObj.username">
                     </div>
                     <div class="my-5">
                         <label class="font-bold">Name</label><br>
                         <input maxlength="200" class="ann-name bg-InputColor drop-shadow-md h-8 w-full rounded-lg"
-                            type="text" v-model.trim="userObj.name">
+                            type="text" v-model="userObj.name">
                     </div>
                     <div class="my-5">
                         <label class="font-bold">Email</label><br>
                         <input maxlength="200" class="ann-email bg-InputColor drop-shadow-md h-8 w-full rounded-lg"
-                            type="text" v-model.trim="userObj.email">
+                            type="text" v-model="userObj.email">
                     </div>
                     <div class="my-5">
                         <label class="font-bold">Role</label><br>
@@ -78,7 +78,7 @@ const save = async (user) => {
                         <button :disabled="checkUserChange"
                             class="ann-button ml-5 shadow-md font-bold rounded-full px-5 py-2 buttonEdit bg-DarkGreen hover:bg-ButtonViewHover"
                             :style="checkUserChange ? 'opacity: 0.5; background-color:lightgray; cursor: not-allowed;' : 'opacity: 1;'"
-                            @click="save(userObj)">Save</button>
+                            @click="save(userObj)">save</button>
                         <router-link :to="{ name: 'userManagement' }">
                             <button
                                 class="ann-button shadow-md rounded-full bg-DarkRed px-6 py-2 ml-3 font-bold hover:bg-ButtonDeleteHover">Cancel</button></router-link>
