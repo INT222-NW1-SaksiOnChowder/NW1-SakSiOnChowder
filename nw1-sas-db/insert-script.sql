d6b43c28d1a052caebdea46572382ee244fa41da
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-08-21 21:20:27 +0700

    add data

--- nw1-sas-db/insert-script.sql
+++ nw1-sas-db/insert-script.sql
@@ -73,6 +73,12 @@ insert into Announcements (announcementTitle,announcementDescription, categoryId
 truncate Users;
 insert into Users(username,name,email,role) values("sanit","Sanit Sirisawatvatana","sanit.sir@kmutt.ac.th","admin");
 insert into Users(username,name,email,role) values("pornthip","Pornthip Sirijutikul","pornthip.sri@kmutt.ac.th","announcer");
-insert into Users(username,name,email,role) values("jaruwan_w","Jaruwan Maneesart","jariwan.wee@kmutt.ac.th","announcer");
+insert into Users(username,name,email,role) values("jaruwan_w","Jaruwan Maneesart","jaruwan.wee@kmutt.ac.th","announcer");
 insert into Users(username,name,email,role) values("vichchuda","Vichchuda Tedoloh","vichchuda.ted@kmutt.ac.th","announcer");
+
+insert into Users(username,name,email,role,createdOn,updatedOn) values("sanit","Sanit Sirisawatvatana","sanit.sir@kmutt.ac.th","admin","2023-08-15 08:00:00+07:00","2023-08-15 08:00:00+07:00");
+insert into Users(username,name,email,role,createdOn,updatedOn) values("pornthip","Pornthip Sirijutikul","pornthip.sri@kmutt.ac.th","announcer","2023-08-15 09:30:00+07:00","2023-08-15 09:30:00+07:00");
+insert into Users(username,name,email,role,createdOn,updatedOn) values("jaruwan_w","Jaruwan Maneesart","jaruwan.wee@kmutt.ac.th","announcer","2023-08-16 08:00:00+07:00","2023-08-16 08:00:00+07:00");
+insert into Users(username,name,email,role,createdOn,updatedOn) values("vichchuda","Vichchuda Tedoloh","vichchuda.ted@kmutt.ac.th","announcer","2023-08-16 09:30:00+07:00","2023-08-16 09:30:00+07:00");
+
 select * from Users;
