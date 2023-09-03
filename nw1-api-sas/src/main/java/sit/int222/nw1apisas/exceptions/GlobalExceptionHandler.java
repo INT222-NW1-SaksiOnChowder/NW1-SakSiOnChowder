0609028c7fbb28763f241e4d2e44a4145db51e10
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-09-03 09:36:19 +0700

    remove unused code in exception class

--- nw1-api-sas/src/main/java/sit/int222/nw1apisas/exceptions/GlobalExceptionHandler.java
+++ nw1-api-sas/src/main/java/sit/int222/nw1apisas/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package sit.int222.nw1apisas.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -21,16 +20,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleValidationAndParsingExceptions(Exception ex, WebRequest request) {
         String title = "Announcement attributes validation failed";
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), title, request.getDescription(false));
-
-        if (ex instanceof MethodArgumentNotValidException) {
-            BindingResult bindingResult = ((MethodArgumentNotValidException) ex).getBindingResult();
-            for (ObjectError field : bindingResult.getAllErrors()) {
-                String fieldName = field.getCode();
-                if (field instanceof FieldError) {
-                    fieldName = ((FieldError) field).getField();
-                }
-                errorResponse.addValidationError(fieldName, field.getDefaultMessage());
+        BindingResult bindingResult = ((MethodArgumentNotValidException) ex).getBindingResult();
+        for (ObjectError field : bindingResult.getAllErrors()) {
+            String fieldName = field.getCode();
+            if (field instanceof FieldError) {
+                fieldName = ((FieldError) field).getField();
             }
+            errorResponse.addValidationError(fieldName, field.getDefaultMessage());
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
