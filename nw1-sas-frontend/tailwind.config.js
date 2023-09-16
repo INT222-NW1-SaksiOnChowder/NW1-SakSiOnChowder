9edb9f485505003a499818177d05197f3df90d6c
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-09-16 15:02:04 +0700

    fix tailwind config

--- nw1-sas-frontend/tailwind.config.js
+++ nw1-sas-frontend/tailwind.config.js
@@ -2,23 +2,23 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
+      colors: {
+        "Background" : "#E4E5E9",
+        "LightBlue" : "#D1D8DE",
+        "DarkBlue" : "#A0B1C1",
+        "DarkGreen" : "#999B86",
+        "DarkRed" : "#BD6666",
+        "BlueFonts" : "#1C3150",
+        "Cream" : "#F0F0F0",
+        "ButtonViewHover" : "#C7C0B6",
+        "ButtonDeleteHover" : "#DCACAC",
+        "InputColor" : "#F0F0F0"
+      }
     },
-    colors: {
-      "Background" : "#E4E5E9",
-      "LightBlue" : "#D1D8DE",
-      "DarkBlue" : "#A0B1C1",
-      "DarkGreen" : "#999B86",
-      "DarkRed" : "#BD6666",
-      "BlueFonts" : "#1C3150",
-      "Cream" : "#F0F0F0",
-      "ButtonViewHover" : "#C7C0B6",
-      "ButtonDeleteHover" : "#DCACAC",
-      "InputColor" : "#F0F0F0"
-    }
   },
   plugins: [require("daisyui")],
 }
