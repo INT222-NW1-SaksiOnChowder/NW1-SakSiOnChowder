1fb5b1295c057d18d7bb39eea8922a11c0c86610
pupha2546 <pupha.2003@mail.kmutt.ac.th>
2023-09-27 19:24:55 +0700

    login

---
+++ nw1-sas-frontend/src/composable/users/getToken.js
@@ -0,0 +1,48 @@
+const ROOT_API = import.meta.env.VITE_ROOT_API
+const getToken = async(user) => {
+    const result = {
+        status : false,
+        message : 'Error',
+        accessToken : '',
+        refreshToken : ''
+    }
+        try {
+            const res = await fetch(`${ROOT_API}/api/token`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(user)
+                }
+            )
+            if (res.status === 200) {
+                result.status = true
+                result.message = 'Password Matched'
+                result.accessToken = res.json().token
+                result.refreshToken = res.json().refreshToken
+                return result
+            }else if(res.status === 401){
+                const error = await res.json()
+                // for(const err of error.detail){
+                //     alert(err.errorMessage)
+                // }
+                result.status = false
+                result.message = 'Password NOT Matched'
+                return result
+            }else if(res.status === 404){
+                const error = await res.json()
+                // for(const err of error.detail){
+                //     alert(err.errorMessage)
+                // }
+                result.status = false
+                result.message = 'The specified username DOES NOT exist'
+                return result
+            }
+        } catch (error) {
+            console.log(`ERROR cannot create data: ${error}`);
+            return result
+        }
+}
+
+export { getToken }
