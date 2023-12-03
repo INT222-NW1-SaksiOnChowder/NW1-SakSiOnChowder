0ad9b1bb21eb2134d09ef3308602fe55cfb26a7e
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-12-03 14:02:36 +0700

    file

--- nw1-sas-frontend/src/composable/announcements/addFiles.js
+++ nw1-sas-frontend/src/composable/announcements/addFiles.js
@@ -2,9 +2,13 @@ import { getNewAccessToken } from "../users/getToken.js"
 
 const ROOT_API = import.meta.env.VITE_ROOT_API
 // const addFiles = async (id, files) => {
+//     // const filesArray = Array.from(files)
+//     // console.log(filesArray)
+//     const arr = Array.from(files)
+//     console.log(arr[0])
 //     const accessToken = localStorage.getItem("accessToken")
 //     try {
-//         const res = await fetch(`${ROOT_API}/api/files?file=${files}?announcementId=${id}`,
+//         const res = await fetch(`${ROOT_API}/api/files?file=${arr[0]}&announcementId=${id}`,
 //             {
 //                 method: 'POST',
 //                 headers: {
@@ -33,42 +37,78 @@ const ROOT_API = import.meta.env.VITE_ROOT_API
 //     }
 // }
 
+// const addFiles = async (id, files) => {
+//     const accessToken = localStorage.getItem("accessToken");
+//     const formData = new FormData();
+
+//     for (let i = 0; i < files.length; i++) {
+//         formData.append('file', files[i]);
+//     }
+
+//     formData.append('announcementId', id);
+
+//     try {
+//         const res = await fetch(`${ROOT_API}/api/files`, {
+//             method: 'POST',
+//             headers: {
+//                 "Authorization": `Bearer ${accessToken}`
+//             },
+//             body: formData
+//         });
+
+//         if (res.ok) {
+//             const result = await res.json();
+//             console.log(result);
+//             console.log('Create successfully');
+//             return true;
+//         } else {
+//             const error = await res.json();
+//             for (const err of error.detail) {
+//                 alert(err.field + " " + err.errorMessage);
+//             }
+//             return false;
+//         }
+//     } catch (error) {
+//         console.log(`ERROR cannot create data: ${error}`);
+//         await getNewAccessToken();
+//         return false;
+//     }
+// };
+
 const addFiles = async (id, files) => {
     const accessToken = localStorage.getItem("accessToken");
+    
+    // const arrFiles = Array.from(files)
+
+    // Create a new FormData object
     const formData = new FormData();
 
-    for (let i = 0; i < files.length; i++) {
-        formData.append('file', files[i]);
-    }
+    // Append files to the FormData object
+    formData.append("file",files)
 
-    formData.append('announcementId', id);
+    // Append announcementId to the FormData object
+    formData.append("announcementId", id);
 
     try {
         const res = await fetch(`${ROOT_API}/api/files`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                "Authorization": `Bearer ${accessToken}`
+                Authorization: `Bearer ${accessToken}`,
             },
-            body: formData
+            body: formData,
         });
 
-        if (res.ok) {
-            const result = await res.json();
-            console.log(result);
-            console.log('Create successfully');
-            return true;
+        if (!res.ok) {
+            console.error(`Error: ${res.status} - ${res.statusText}`);
         } else {
-            const error = await res.json();
-            for (const err of error.detail) {
-                alert(err.field + " " + err.errorMessage);
-            }
-            return false;
+            const data = await res.json();
+            console.log(data);
         }
     } catch (error) {
-        console.log(`ERROR cannot create data: ${error}`);
-        await getNewAccessToken();
-        return false;
+        console.error("Error uploading files:", error);
     }
 };
+
+
 
 export { addFiles }
